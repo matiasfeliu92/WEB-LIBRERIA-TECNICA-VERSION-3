a6d07cd46aa544a76cef502a4e16a7f13989787c
matiasfeliu92 <matumazparrote@gmail.com>
2021-10-12 21:04:37 -0300

    ajustes web libreria tecnica con JQUERY

--- pruebas.js
+++ pruebas.js
@@ -36,6 +36,9 @@ console.log(precioTotal)
 
 const carrito = []
 
-$(stockProductos).each(function(index, producto) => {
-
+$(stockProductos).each(function(producto, index) => {
+    $('#contenedor-productos').append(`<div class="producto"><h3>${producto.nombre}</h3>
+                <img src=${producto.img} alt="">
+                <p class="precioProducto">Precio: $${producto.precio}</p>
+                <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar al carrito <i class="fas fa-shopping-cart"></i></button></div>`)
 })
