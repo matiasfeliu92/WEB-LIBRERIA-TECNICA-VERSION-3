bb8163be59ccef0b6a2213750cc179e7733ab6cd
matiasfeliu92 <matumazparrote@gmail.com>
2021-10-14 12:39:12 -0300

    avances web libreria con JQUERY

--- pruebas.js
+++ pruebas.js
@@ -1,38 +1,14 @@
-const contenedorModal = $('.modal-contenedor')
-console.log(contenedorModal)
-
-const botonAbrir = $('#boton-carrito')
-console.log(botonAbrir)
-
-const botonCerrar = $('#carritoCerrar')
-console.log(botonCerrar)
-
-const modalCarrito = $('.modal-carrito')
-console.log(modalCarrito)
-
-botonAbrir.click(function() {
-    contenedorModal.addClass('modal-active');
-});
-
-botonCerrar.click(function() {
-    contenedorModal.removeClass('modal-active')
+$('#boton-carrito').click(() => {
+    $('.modal-contenedor').addClass('modal-active')
 })
 
-modalCarrito.click(function(event) {
+$('#carritoCerrar').click(() => {
+    $('.modal-contenedor').removeClass('modal-active')
+})
+
+$('.modal-carrito').click((event) => {
     event.stopPropagation()
 })
-
-const contenedorProductos = $('#contenedor-productos')
-console.log(contenedorProductos)
-
-const contenedorCarrito = $('#carrito-contenedor')
-console.log(contenedorCarrito)
-
-const contadorCarrito = $('#contadorCarrito')
-console.log(contadorCarrito)
-
-const precioTotal = $('#precioTotal')
-console.log(precioTotal)
 
 const carrito = []
 
