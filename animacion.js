ab623fe503fd03090f1d1b1bdc84f26a9bf7778c
matiasfeliu92 <matumazparrote@gmail.com>
2021-10-18 10:53:02 -0300

    avances de animacion JQUERY para desafio complementario

--- animacion.js
+++ animacion.js
@@ -1,38 +1,10 @@
-// $(document).ready(() => {
-//     $('#texto-animado')
-//         .animate({ "color": "red", }, 500)
-//         .animate({ "color": "blue" }, 500)
-//         .fadeOut(13000)
-
-// })
-
-// $(document).ready(() => {
-//     $(".presentacion").click(function() {
-//         $(".presentacion").animate({ "top": "+=50%" }, 2000, "swing", function() {
-//             $(".presentacion").css({ "border": "1px solid black", "width": "100%", "height": "100%" })
-//                 .slideUp(2000)
-//                 .slideDown(2000)
-//                 .slideUp(2000)
-//                 .slideDown(2000)
-//                 .slideUp(2000)
-//                 .slideDown(2000)
-//                 .fadeOut(1000)
-//         });
-
-//     });
-// })
-
-
 $(document).ready(() => {
-    $(".presentacion").click(function() {
-        $(".presentacion").animate({ "color": "blue" }, 2000, "swing", function() {
-            $(".presentacion").css({ "color": "blue" })
-                .slideUp(2000)
-                .slideDown(2000)
-                .slideUp(2000)
-                .slideDown(2000)
-                .fadeOut(1000)
-        });
 
-    });
+    $(".presentacion").css({ "background-color": "gray" })
+        .slideUp(2000)
+        .slideDown(2000)
+        .slideUp(2000)
+        .slideDown(2000)
+        .fadeOut(1000)
+
 })
