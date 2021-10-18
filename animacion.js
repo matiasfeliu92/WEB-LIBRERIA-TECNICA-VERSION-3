9c5e4c1ce13aac50acda02a5cd98684f7020e8eb
matiasfeliu92 <matumazparrote@gmail.com>
2021-10-18 13:49:11 -0300

    diseño responsive del sitio

--- animacion.js
+++ animacion.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
 
-    $(".presentacion").css({ "background-color": "gray" })
+    $(".presentacion").css({ "background-color": "gray", "color": "black", "font-weight": "bolder" })
         .slideUp(2000)
         .slideDown(2000)
         .slideUp(2000)
