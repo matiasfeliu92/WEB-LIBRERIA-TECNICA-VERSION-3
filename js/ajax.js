6a4b36525b5a62707b199f34046abf12520a3625
matiasfeliu92 <matumazparrote@gmail.com>
2021-10-20 13:10:37 -0300

    ajustes y avances usando AJAX

--- js/ajax.js
+++ js/ajax.js
@@ -21,4 +21,42 @@ $.getJSON(URL, (response, success) => {
     localStorage.setItem('productos', productosToString)
 
     console.log(response)
+
+    const agregarAlCarrito = (item) => {
+
+        const productoEnCarrito = carrito.find((prod) => prod.id === item)
+
+        if (productoEnCarrito) {
+            productoEnCarrito.cantidad++
+        } else {
+
+            const producto = response.find((prod) => prod.id === item)
+
+            carrito.push({ id: producto.id, nombre: producto.nombre, precio: producto.precio, cantidad: 1 })
+        }
+
+        console.log(carrito)
+        actualizarCarrito()
+    }
+
+    const actualizarCarrito = () => {
+        $('#carrito-contenedor').html("")
+
+        carrito.forEach((prod) => {
+            $('#carrito-contenedor').append(
+                `
+                <div class= "productoEnCarrito">
+                    <p>${prod.nombre}</p>
+                    <p>Precio: $${prod.precio}</p>
+                    <p>Cantidad: ${prod.cantidad}</p>
+                    <button onclick="eliminarProducto(${prod.id})" class="boton-eliminar">-</button>
+                </div>
+                 `
+            )
+        })
+
+        $('#contadorCarrito').html(carrito.reduce((acc, prod) => acc += prod.cantidad, 0))
+        $('#precioTotal').html(carrito.reduce((acc, prod) => acc += prod.precio * prod.cantidad, 0))
+    }
+
 })
